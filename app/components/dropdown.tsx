0fb34759b985669433c15148bf2c91c31f912558
zaoying <505155730@qq.com>
2023-07-21 01:03:59 +0800

    待完善的组件

---
+++ app/components/dropdown.tsx
@@ -0,0 +1,31 @@
+import { useIoC } from 'Com/app/hooks/ioc';
+import { FC, ReactNode, useState } from 'react';
+
+const {define, inject} = useIoC()
+
+export type Trigger = "hover" | "click"
+export type DropdownProps = {
+    trigger?: Trigger
+    children: ReactNode
+}
+
+export const Dropdown: FC<DropdownProps> = define((props) => {
+    const [collapse, setCollapse] = useState(true)
+    return <div className={`dropdown ${collapse ? "collapse" : "show"}`}>
+        {props.children}
+    </div>
+})
+
+export interface DropdownController {
+
+}
+
+export function useDropdown(component?: FC<DropdownProps>, trigger?: Trigger): [ReactNode, DropdownController] {
+    const dropdown = component ?? inject(Dropdown)
+    const [props, setProps] = useState<DropdownProps>({
+        trigger: trigger ?? "hover",
+        children: <></>
+    })
+    const ctl: DropdownController = {}
+    return [dropdown(props), ctl]
+}
